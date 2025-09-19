7b61449eedbb960e19e24d1db3f0a47b508a2bc1
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2025-09-19 11:46:16 +0330

    feat: update and add tasks mdoel for task form

--- src/app/core/models/event.model.ts
+++ src/app/core/models/event.model.ts
@@ -1,9 +1,19 @@
-export interface IEvent {
+import { FormControl } from '@angular/forms';
+
+export interface ITask {
   id: string;
   title: string;
-  start: Date;
-  end: Date;
+  duration: number;
   allDay: boolean;
   description: string;
   location: string;
+}
+
+export interface ITaskForm {
+  id: FormControl<string>;
+  title: FormControl<string>;
+  duration: FormControl<number>;
+  allDay: FormControl<boolean>;
+  description: FormControl<string>;
+  location: FormControl<string>;
 }
