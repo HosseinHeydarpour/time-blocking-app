80963ab9c9be70f512106d9d59d14cd61b81f24e
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2025-09-19 10:22:29 +0330

    feat: add home route

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MobileNav } from './layout/mobile-nav/mobile-nav';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MobileNav],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('time-blocking');
