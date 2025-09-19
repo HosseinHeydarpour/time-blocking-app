bc7810bd80f3ef9d3aac73a7bc557daaca23a520
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2025-09-19 20:45:02 +0330

    feat(WIP): assign tasks to existing blocks

--- src/app/features/home/timing/timing.ts
+++ src/app/features/home/timing/timing.ts
@@ -1,4 +1,13 @@
-import { Component, inject, OnDestroy, OnInit } from '@angular/core';
+import {
+  Component,
+  effect,
+  ElementRef,
+  inject,
+  OnDestroy,
+  OnInit,
+  QueryList,
+  ViewChildren,
+} from '@angular/core';
 import { SettingsService } from '../../../core/services/settings-service';
 import { Subscription } from 'rxjs';
 import { CLIENT_RENEG_LIMIT } from 'tls';
@@ -16,8 +25,23 @@ export class Timing implements OnInit, OnDestroy {
   settings: any;
   subscriptions: Subscription = new Subscription();
   taskService = inject(TaskService);
+  tasks: any[] = [];
 
-  constructor() {}
+  @ViewChildren('listItem') listItems!: QueryList<ElementRef>;
+
+  constructor() {
+    effect(() => {
+      this.tasks = this.taskService.tasks().map((task) => {
+        const taskDuration = this.taskService.durationToMinutes(task.duration);
+        return {
+          task: task.title,
+          duration: taskDuration,
+        };
+      });
+      console.log(this.tasks);
+      if (this.listItems) this.assignTasks();
+    });
+  }
   ngOnInit(): void {
     this.settings = this.settingsService.fetchSettings();
     this.subscriptions.add(
@@ -56,5 +80,41 @@ export class Timing implements OnInit, OnDestroy {
       .padStart(2, '0');
     const minutes = (totalMinutes % 60).toString().padStart(2, '0');
     return `${hours}:${minutes}`;
+  }
+
+  assignTasks() {
+    console.log('ASSIGN CALLED');
+    const takenBlocks: Set<number> = new Set(); // track used blocks
+
+    console.log(this.listItems);
+    this.listItems.forEach((item) => {
+      item.nativeElement.classList.remove('taken');
+      item.nativeElement.style.backgroundColor = '';
+    });
+
+    this.tasks.forEach((task) => {
+      const blocksNeeded = Math.ceil(task.duration / this.settings.timeBlockDuration);
+
+      let assigned = 0;
+      this.listItems.forEach((item, index) => {
+        if (assigned >= blocksNeeded) return; // done for this task
+        if (!takenBlocks.has(index)) {
+          takenBlocks.add(index);
+          assigned++;
+          item.nativeElement.classList.add('taken');
+          item.nativeElement.style.backgroundColor = this.getRandomColor();
+          item.nativeElement.textContent = `${task.task} - ${task.duration} minutes`;
+        }
+      });
+    });
+  }
+
+  getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
   }
 }
