071830230bc7c78fa6b4ea98b3805667916fd558
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2025-09-19 17:15:37 +0330

    feat: calculate timing blocks based on start and end and block

--- src/app/features/home/timing/timing.ts
+++ src/app/features/home/timing/timing.ts
@@ -15,4 +15,30 @@ export class Timing implements OnInit {
   ngOnInit(): void {
     this.settings = this.settingsService.fetchSettings();
   }
+
+  calculateTimingBlocks() {
+    const { startAt, endAt, timeBlockDuration } = this.settings;
+
+    const start = this.parseTime(startAt); // in minutes
+    const end = this.parseTime(endAt); // in minutes
+    const blocks: string[] = [];
+
+    for (let time = start; time < end; time += timeBlockDuration) {
+      blocks.push(this.formatTime(time));
+    }
+
+    return blocks;
+  }
+  private parseTime(time: string): number {
+    const [hours, minutes] = time.split(':').map(Number);
+    return hours * 60 + minutes;
+  }
+
+  private formatTime(totalMinutes: number): string {
+    const hours = Math.floor(totalMinutes / 60)
+      .toString()
+      .padStart(2, '0');
+    const minutes = (totalMinutes % 60).toString().padStart(2, '0');
+    return `${hours}:${minutes}`;
+  }
 }
